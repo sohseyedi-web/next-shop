5ad0108cfa78ff4cc3bcf01fa05bbedcd041efe8
sohseyedi <sohseyedi@gmail.com>
2023-07-31 17:41:47 +0330

    bug fixed

--- src/app/(user)/[id]/page.jsx
+++ src/app/(user)/[id]/page.jsx
@@ -1,5 +1,4 @@
-import ProductDetail from "./productDetail";
-
+import ProductDetail from "./ProductDetail";
 const Product = () => {
   return (
     <section className="pt-28 max-w-7xl mx-auto md:px-0 px-3">
